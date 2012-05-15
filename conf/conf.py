cf29ed5ff4825e630761ad3ddfe327d2aabd37c4
Ronald Kaiser <raios.catodicos@gmail.com>
2012-05-15 13:31:24 -0300

    simplyfing path

--- conf/conf.py
+++ conf/conf.py
@@ -10,6 +10,6 @@ import sys
 
 # change to your personal jeppeto path
 JEPPETO_PATH = "/home/kaiser/workspace/jeppeto"
-PATH_DELIMITER = '/'
 
-sys.path.append(os.path.join(JEPPETO_PATH.split(PATH_DELIMITER)))
+
+sys.path.append(JEPPETO_PATH)
