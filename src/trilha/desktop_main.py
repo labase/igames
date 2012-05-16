207db9bfcf8614bd447a390722c28de9faa1216b
Ronald Kaiser <raios.catodicos@gmail.com>
2012-05-16 15:54:15 -0300

    removing import

--- src/trilha/desktop_main.py
+++ src/trilha/desktop_main.py
@@ -20,7 +20,7 @@ __date__    = "2012/05/11 $Date$"
 
 import os
 if __name__ == '__main__':
-    import os, sys
+    import sys
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     import __meta__
     __meta__.add_jeppeto_into_path()
